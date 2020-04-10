3a74245ae89b442bda4891fe8af019a93c9702db
Colin But <colin.but@outlook.com>
2020-04-10 16:12:10 +0100

    exporting the subnet id from network

--- mgmt/network/main.tf
+++ mgmt/network/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
     name = "sales-order-system-mgmt-vpc"
     cidr = "10.0.0.0/16"
 
-    azs = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+    azs = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
     public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     
     enable_nat_gateway = false
--- mgmt/network/outputs.tf
+++ mgmt/network/outputs.tf
@@ -1,3 +1,7 @@
 output "jenkins_security_group_id" {
     value = aws_security_group.jenkins_security_group.id
 }
+
+output "vpc_network_subnet1_id" {
+    value = module.vpc.public_subnets[0]
+}
