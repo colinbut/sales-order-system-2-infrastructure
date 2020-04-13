6934239ecc3c6e953f9d428b546ad4f955520319
Colin But <colin.but@outlook.com>
2020-04-13 22:46:22 +0100

    provision elasticache

---
+++ live/dev/data-stores/product-data-store/main.tf
@@ -0,0 +1,55 @@
+terraform {
+    backend "s3" {
+        bucket  = "sales-order-system-terraform-state-s3-bucket"
+        key     = "live/dev/data-stores/product-data-store/terraform.tfstate"
+        region  = "eu-west-2"
+    }
+}
+
+provider "aws" {
+    region = "eu-west-1"
+}
+
+data "terraform_remote_state" "app_network" {
+    backend = "s3"
+    config = {
+        bucket  = "sales-order-system-terraform-state-s3-bucket"
+        key     = "live/dev/app-network/terraform.tfstate"
+        region  = "eu-west-2"
+    }
+}
+
+locals {
+    redis_port = 6379
+}
+
+resource "aws_security_group" "redis_elasticache_security_group" {
+    name        = "redis_elasticache_security_group"
+    description = "The security group for the redis elasticache"
+    vpc_id      = data.terraform_remote_state.app_network.outputs.vpc_id
+
+    ingress {
+        from_port   = local.redis_port
+        to_port     = local.redis_port
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_elasticache_subnet_group" "elasticache_redis_subnet_group" {
+    name        = "redis-subnet-group"
+    description = "Subnet Group for redis elasticache"
+    subnet_ids  = [lookup(data.terraform_remote_state.app_network.outputs.subnets, "app_private_subnet_a").id]
+}
+
+resource "aws_elasticache_cluster" "product_redis_elasticache_cluster" {
+    cluster_id              = "product-redis-data-store"
+    engine                  = "redis"
+    engine_version          = "5.0.6"
+    num_cache_nodes         = 1
+    node_type               = "cache.t2.micro"
+    parameter_group_name    = "default.redis5.0"
+    security_group_ids      = [aws_security_group.redis_elasticache_security_group.id]
+    subnet_group_name       = aws_elasticache_subnet_group.elasticache_redis_subnet_group.name
+    port                    = local.redis_port
+}
---
+++ live/dev/data-stores/product-data-store/outputs.tf
@@ -0,0 +1,3 @@
+output "cache_nodes" {
+    value = aws_elasticache_cluster.product_redis_elasticache_cluster.cache_nodes
+}
