bcfcc6f67221b921cf607d03487b66ca85654760
Colin But <colin.but@outlook.com>
2020-04-13 17:02:47 +0100

    fixing state paths for the new microservices

--- live/dev/backend-services/product-service/main.tf
+++ live/dev/backend-services/product-service/main.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket          = "sales-order-system-terraform-state-s3-bucket"
-        key             = "live/dev/backend-services/user-service/terraform.tfstate"
+        key             = "live/dev/backend-services/product-service/terraform.tfstate"
         region          = "eu-west-2"
         dynamodb_table  = "sales-order-system-S3-state-lock"
     }
