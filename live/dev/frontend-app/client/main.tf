0968398fcb01cd42510932c4771638f14ebf5f95
Colin But <colin.but@outlook.com>
2020-04-13 17:10:35 +0100

    frontend web server

---
+++ live/dev/frontend-app/client/main.tf
@@ -0,0 +1,69 @@
+terraform {
+    backend "s3" {
+        bucket          = "sales-order-system-terraform-state-s3-bucket"
+        key             = "live/dev/frontend-app/client/terraform.tfstate"
+        region          = "eu-west-2"
+        dynamodb_table  = "sales-order-system-S3-state-lock"
+    }
+}
+
+provider "aws" {
+    region = "eu-west-1"
+}
+
+data "terraform_remote_state" "app_network" {
+    backend = "s3"
+    config = {
+        bucket  = "sales-order-system-terraform-state-s3-bucket"
+        key     = "live/dev/app-network/terraform.tfstate"
+        region  = "eu-west-2"
+    }
+}
+
+locals {
+    ami             = var.ami
+    key_pair        = var.key_pair
+    server_name     = var.server_name
+    environment     = var.environment
+    http_port       = var.http_port
+    ssh_port        = 22
+    protocol        = "tcp"
+    any_port        = 0
+    any_protocol    = "-1"
+}
+
+module "frontend-webserver-server" {
+    source      = "../../../../modules/backend-servers"
+
+    ami         = local.ami
+    key_pair    = local.key_pair
+    server_name = "${local.server_name}-${local.environment}"
+    security_groups = aws_security_group.security_group
+}
+
+resource "aws_security_group" "security_group" {
+    name        = "${local.server_name}_security_group"
+    description = "The SG for normal web access & SSH from outside"
+    vpc_id      = data.terraform_remote_state.app_network.outputs.vpc_id
+
+    ingress {
+        from_port   = local.http_port
+        to_port     = local.http_port
+        protocol    = local.protocol
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = local.ssh_port
+        to_port     = local.ssh_port
+        protocol    = local.protocol
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = local.any_port
+        to_port     = local.any_port
+        protocol    = local.any_protocol
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
---
+++ live/dev/frontend-app/client/terraform.tfvars
@@ -0,0 +1,5 @@
+ami             = "ami-06ce3edf0cff21f07"
+key_pair        = "MyIrelandKP"
+server_name     = "sales-order-system-app"
+http_port       = 80
+environment     = "dev"
