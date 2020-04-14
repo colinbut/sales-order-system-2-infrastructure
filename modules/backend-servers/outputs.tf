6d889bad789a658be4e22cbbb32a52973c728590
Colin But <colin.but@outlook.com>
2020-04-14 23:37:14 +0100

    fixing output of private ip and dns for backend services

--- modules/backend-servers/outputs.tf
+++ modules/backend-servers/outputs.tf
@@ -5,3 +5,11 @@ output "public_ip_address" {
 output "public_url" {
     value = aws_instance.server.public_dns
 }
+
+output "private_ip_address" {
+    value = aws_instance.server.private_ip
+}
+
+output "private_url" {
+    value = aws_instance.server.private_dns
+}
