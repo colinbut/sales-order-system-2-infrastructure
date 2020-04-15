f38b35cd6bf035d984dbb08048619dc369aaf73b
Colin But <colin.but@outlook.com>
2020-04-15 20:32:39 +0100

    output private ip and dns on user service

--- live/dev/backend-services/user-service/outputs.tf
+++ live/dev/backend-services/user-service/outputs.tf
@@ -1,7 +1,7 @@
-output "public_ip_address" {
-    value = module.backend-server.public_ip_address
+output "private_ip_address" {
+    value = module.backend-server.private_ip_address
 }
 
-output "public_dns_url" {
-    value = module.backend-server.public_url
+output "private_dns_url" {
+    value = module.backend-server.private_url
 }
