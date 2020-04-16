88040e1c06b63ccb732badab71dfcdcb1cc3700d
Colin But <colin.but@outlook.com>
2020-04-16 12:10:34 +0100

    output public ip and dns for bastian

--- live/dev/app-network/outputs.tf
+++ live/dev/app-network/outputs.tf
@@ -4,4 +4,12 @@ output "vpc_id" {
 
 output "subnets" {
     value = module.app_public_subnets.subnets
-} 
+}
+
+output "public_ip_address" {
+    value = module.backend-server.public_ip_address
+}
+
+output "public_dns_url" {
+    value = module.backend-server.public_url
+}
